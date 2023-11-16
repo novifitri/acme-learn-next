afb5a18ac060b8eff8dce7a0f8225a8041186e5f
novi.fitri <101563676+novifitri@users.noreply.github.com>
2023-11-16 14:27:30 +0700

    fix: change font to fonts

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import styles from '@/app/ui/home.module.css'
 
 import Link from 'next/link'
-import { lusitana } from './ui/font'
+import { lusitana } from './ui/fonts'
 import Image from 'next/image'
 
 export default function Page() {
